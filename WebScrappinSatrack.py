7449203224c86a3b4619c28b091f1d7cd5c52e80
Marcosespa <marcosespa24@gmail.com>
2024-12-10 14:45:51 -0500

    prieba otra vez

--- WebScrappinSatrack.py
+++ WebScrappinSatrack.py
@@ -1,6 +1,7 @@
 import ssl
 import time
 import undetected_chromedriver as uc
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
